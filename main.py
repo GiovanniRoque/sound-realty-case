814afc2c2a91c4a024178541ebc2bce1dbe8b3d1
Giovanni Roque <giovannischeneroque@gmail.com>
2025-08-28 11:25:06 -0300

    feat: basic predict route

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import pickle 
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
 
 import pandas as pd
 
@@ -19,6 +20,21 @@ MODEL_PATH = os.path.join(MODEL_DIR, 'model.pkl')
 FEATURES_PATH = os.path.join(MODEL_DIR, 'model_features.json')
 DEMOGRAPHICS_PATH = os.path.join(DATA_DIR, 'zipcode_demographics.csv')
 
+# Response model
+class PredictionResponse(BaseModel):
+    predicted_price: float
+
+# Request model
+class HouseDataRequest(BaseModel):
+    bedrooms: float
+    bathrooms: float
+    sqft_living: float
+    sqft_lot: float
+    floors: float
+    sqft_above: float
+    sqft_basement: float
+    zipcode: str
+
 
 class ModelLoader:
     """Encapsulates the loaded model, features and demographics data."""
@@ -32,7 +48,7 @@ class ModelLoader:
         """Loads the model, feature list, and demographics data."""
         try:
             with open(MODEL_PATH, 'rb') as f:
-                self.model = pickle.load(f)
+                self.model: Pipeline = pickle.load(f)
             logger.info(f"Model loaded: {MODEL_PATH}")
 
             with open(FEATURES_PATH, 'r') as f:
@@ -65,4 +81,35 @@ app = FastAPI(title="Sound Realty House Price Prediction API",
 @app.on_event("startup")
 async def startup_event():
     # Load model when model starts
+    logger.info("Starting up application")
     model_loader.load_model_and_data()
+    logger.info("Application startup complete!")
+
+@app.post("/predict", response_model=PredictionResponse, status_code=status.HTTP_200_OK)
+async def predict(house_data: HouseDataRequest):
+
+    if not model_loader.loaded:
+        logger.error("Model and data not loaded")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Model or data not initialized")
+
+    # Access model from loader
+    model = model_loader.model
+    model_features = model_loader.model_features
+    demographics_df = model_loader.demographics_df
+
+    try:
+        # Merge the input data to the demographics dataframe as it was done on create_model
+        input_house_data = pd.DataFrame([house_data.model_dump()])
+        merged_house_data = input_house_data.merge(demographics_df, how="left", left_on='zipcode', right_index=True).drop(columns=['zipcode'])
+        logger.info(f"Merged dataframe shape: {merged_house_data.shape}")
+
+        prediction = model.predict(merged_house_data)[0]
+        logger.info("Prediction successful!")
+
+        return PredictionResponse(predicted_price=float(prediction))
+    except Exception as e:
+        # Generic error for initial debugging
+        logger.exception(f"Unexpected error during prediction: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="An internal error occurred during prediction")
